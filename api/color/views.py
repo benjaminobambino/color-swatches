2ada0ff055f733fbb3a60dcdd22f30aa694482d3
Benjamin Peck <benjaminlpeck@gmail.com>
2022-03-13 16:49:20 -0700

    added views

--- api/color/views.py
+++ api/color/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ColorSerializer, SwatchSerializer
+from .models import Color, Swatch
 
-# Create your views here.
+class ColorList(generics.ListCreateAPIView):
+  queryset = Color.objects.all()
+  serializer_class = ColorSerializer
+
+class ColorDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Color.objects.all()
+  serializer_class = ColorSerializer
+
+class SwatchList(generics.ListCreateAPIView):
+  queryset = Swatch.objects.all()
+  serializer_class = SwatchSerializer
+
+class SwatchDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Swatch.objects.all()
+  serializer_class = SwatchSerializer
