a438cd0941256be876f38b1014ab70eab89e4831
Benjamin Peck <benjaminlpeck@gmail.com>
2022-03-13 17:05:39 -0700

    configured cors settings

--- api/color_django/settings.py
+++ api/color_django/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'color',
     'rest_framework',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -53,6 +54,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -135,3 +137,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS settings
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000/',
+]
+
+#CSRF settings
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000/',
+]
