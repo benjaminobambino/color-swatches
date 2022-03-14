93cf0465f4880de4d81ac9b9206d24675811b583
Benjamin Peck <benjaminlpeck@gmail.com>
2022-03-13 20:18:28 -0700

    added colors and swatches states and tested with axios get on mount

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [colors, setColors] = useState([]);
+  const [swatches, setSwatches] = useState([]);
+
+  const getColors = async () => {
+    await axios.get('http://localhost:8000/colors/').then((res) => {
+      setColors(res.data);
+    });
+  };
+
+  useEffect(() => {
+    getColors();
+  }, []);
+
+  return <div className="App">{/* <p>{colors}</p> */}</div>;
 }
 
 export default App;
