41bc604322ef36aba9ac66041ac23ee0f63d67b7
Benjamin Peck <benjaminlpeck@gmail.com>
2022-03-14 19:49:02 -0700

    added clear button functionality

--- client/src/components/detail/ClearButton.jsx
+++ client/src/components/detail/ClearButton.jsx
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router-dom'
+
 const ClearButton = () => {
+  const navigate = useNavigate()
+
   return (
-    <button className="clear-button">
+    <button className="clear-button" onClick={() => navigate('/')}>
       Clear
     </button>
   )
