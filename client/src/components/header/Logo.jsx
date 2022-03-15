afa515c13fe5946ea4a7f246d476a73ed102e0d4
Benjamin Peck <benjaminlpeck@gmail.com>
2022-03-15 11:07:40 -0700

    refactored logoLink

--- client/src/components/header/Logo.jsx
+++ client/src/components/header/Logo.jsx
@@ -1,9 +1,10 @@
 const logoSymbol = './logo-symbol.svg';
+const logoLink = 'https://www.helpfulhuman.com/';
 
 const Logo = () => {
   return (
     <div className="logo">
-      <a href="https://www.helpfulhuman.com/" target="blank" rel="noopener noreferrer">
+      <a href={logoLink} target="blank" rel="noopener noreferrer">
         <img src={logoSymbol} alt="logo" />
       </a>
     </div>
