cb692463e4b713e3fa2439b805b10cb31180ad0e
Benjamin Peck <benjaminlpeck@gmail.com>
2022-03-14 20:00:28 -0700

    started random button functionality

--- client/src/components/sidebar/RandomButton.jsx
+++ client/src/components/sidebar/RandomButton.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom'
+
 const RandomButton = () => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate(`/25`)
+  }
+
   return (
     <div>
-      <button className="random-button">Random Color</button>
+      <button className="random-button" onClick={handleClick}>Random Color</button>
     </div>
   )
 }
