2f6388bb6702f8a98c72646beee3b88285647c92
Benjamin Peck <benjaminlpeck@gmail.com>
2022-03-15 11:14:59 -0700

    tidied SwatchCard

--- client/src/components/swatches/SwatchCard.jsx
+++ client/src/components/swatches/SwatchCard.jsx
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom"
 
 const SwatchCard = ({ swatch }) => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
+  const { id, hex } = swatch
 
   return (
-    <div className="swatch-card" onClick={() => navigate(`/${swatch.id}`)}>
-      <section className="swatch-background" style={{ backgroundColor: swatch.hex }}></section>
-      <p>{swatch.hex}</p>
+    <div className="swatch-card" onClick={() => navigate(`/${id}`)}>
+      <section className="swatch-background" style={{ backgroundColor: hex }}></section>
+      <p>{hex}</p>
     </div>
   )
 }
