995bf41149fcafea5c6f314f15659e157526cdb6
Benjamin Peck <benjaminlpeck@gmail.com>
2022-03-15 11:06:29 -0700

    made logo clickable

--- client/src/components/header/Logo.jsx
+++ client/src/components/header/Logo.jsx
@@ -3,7 +3,9 @@ const logoSymbol = './logo-symbol.svg';
 const Logo = () => {
   return (
     <div className="logo">
-      <img src={logoSymbol} alt="logo" />
+      <a href="https://www.helpfulhuman.com/" target="blank" rel="noopener noreferrer">
+        <img src={logoSymbol} alt="logo" />
+      </a>
     </div>
   )
 }
